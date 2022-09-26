3adbaf11c2d8d60ee76481d307e28d89aaa6b7da
slescano2002 <slescano2002@yahoo.com.ar>
2022-09-26 17:42:33 -0300

    26/9/2022 _components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { IdiomasComponent } from './componentes/idiomas/idiomas.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    PerfilComponent,
+    ExperienciaComponent,
+    IdiomasComponent,
+    HabilidadesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
